e66ad9ab93013f9f16ab7cc8235d561d71edd718
Derek S <derek.schindler.2022@studet.lhchools.org>
2019-02-05 07:43:34 -0500

    Created Scanner Class and initialized inches and pounds variable

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,15 @@
+package org.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // equation for BMI is BMI=kg/m*m
+        Scanner keyboard;
+        int inches, pounds;
+        keyboard = new Scanner(System.in);
+        
+
+    }
+}
